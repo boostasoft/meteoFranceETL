c0e7209ff1befadaee596efcb9f6e6d48c765cee
Boostasoft <75505556+boostasoft@users.noreply.github.com>
2021-02-03 01:31:40 +0100

    Update WellformedData.sh

--- WellformedData.sh
+++ WellformedData.sh
@@ -1,20 +1,7 @@
 #! /bin/bash
 # pour faire fonctionner correctement les scripts du bash, on a changé l'entete de /bin/sh a /bin/bash
 PROJET=`pwd`
-# L'interet de ce shell est de permettre la segmentation et le deployement
-# des fichiers sous formes des données xml généréees par yatea
-#La principale difficulté est l'encodage des chaines des caracteres
-#Ce script permet de recuperer les librables placés dans repertoires et
-#un contenu xml de yatea... Nous avons opté de faire un  flemmatisation des contenus treetagger
-#avant des transmettre a yatea.
-# & wait permet d'attendre la fin du processus afin d'executer la suivante.
-# Le todo consistera à mettre ces contenus dans un repertoire temporaire afin
-# afin d'assurer la destruction des repertoire  a tout moment...
-#version du 01/05/2011...
-# Il est difficile de lire tous mes .000 du format
-# s57... le fichier marche pour un document s57...
-# placé dans  datas57....
-#cd ${PROJET}/datas57/
+
 virgule=","
 point="."
 pointvirgule=","
